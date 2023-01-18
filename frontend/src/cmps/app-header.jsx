e0d57519e336068a92da86508c5a7b08ce2a8119
Lior Geni <liorgeni@gmail.com>
2023-01-18 03:22:49 +0200

    layout, header, footer, vars

--- frontend/src/cmps/app-header.jsx
+++ frontend/src/cmps/app-header.jsx
@@ -1,82 +1,78 @@
-import { Link, NavLink } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import routes from '../routes'
-import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
-import { login, logout, signup } from '../store/user.actions.js'
-import { LoginSignup } from './login-signup.jsx'
+import { Link, NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import routes from "../routes";
+import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service";
+import { login, logout, signup } from "../store/user.actions.js";
+import { LoginSignup } from "./login-signup.jsx";
 
 export function AppHeader() {
-    const user = useSelector((storeState) => storeState.userModule.user)
+  const user = useSelector((storeState) => storeState.userModule.user);
 
-    async function onLogin(credentials) {
-        try {
-            const user = await login(credentials)
-            showSuccessMsg(`Welcome: ${user.fullname}`)
-        } catch (err) {
-            showErrorMsg('Cannot login')
-        }
+  async function onLogin(credentials) {
+    try {
+      const user = await login(credentials);
+      showSuccessMsg(`Welcome: ${user.fullname}`);
+    } catch (err) {
+      showErrorMsg("Cannot login");
     }
-    async function onSignup(credentials) {
-        try {
-            const user = await signup(credentials)
-            showSuccessMsg(`Welcome new user: ${user.fullname}`)
-        } catch (err) {
-            showErrorMsg('Cannot signup')
-        }
+  }
+  async function onSignup(credentials) {
+    try {
+      const user = await signup(credentials);
+      showSuccessMsg(`Welcome new user: ${user.fullname}`);
+    } catch (err) {
+      showErrorMsg("Cannot signup");
     }
-    async function onLogout() {
-        try {
-            await logout()
-            showSuccessMsg(`Bye now`)
-        } catch (err) {
-            showErrorMsg('Cannot logout')
-        }
+  }
+  async function onLogout() {
+    try {
+      await logout();
+      showSuccessMsg(`Bye now`);
+    } catch (err) {
+      showErrorMsg("Cannot logout");
     }
+  }
 
-    return (
-        <header className='app-header'>
-            <nav className='flex'>
-                <h1 className='header-logo'>Logo</h1>
-                <input type='text' className='search-box' placeholder='What service are you looking for today?'></input>
-                <div className='search-icon-box'>
-                    <span class='material-symbols-outlined search-icon'>search</span>
-                </div>
-                <span class="material-symbols-outlined">
-notifications
-</span>
-<span class="material-symbols-outlined">
-mail
-</span>
-<span class="material-symbols-outlined">
-favorite
-</span>
-<span>Orders</span>
-<span class="material-symbols-outlined">
-account_circle
-</span>
-                {routes.map((route) => (
-                    <NavLink key={route.path} to={route.path}>
-                        {route.label}
-                    </NavLink>
-                ))}
+  return (
+    <header className="app-header main-layout full flex align-center sticky">
+      <nav className="flex">
+        <h1 className="header-logo">Fiverr</h1>
+        <input
+          type="text"
+          className="search-box"
+          placeholder="What service are you looking for today?"
+        ></input>
+        <div className="search-icon-box">
+          <span class="material-symbols-outlined search-icon">search</span>
+        </div>
+        <span class="material-symbols-outlined">notifications</span>
+        <span class="material-symbols-outlined">mail</span>
+        <span class="material-symbols-outlined">favorite</span>
+        <span>Orders</span>
+        <span class="material-symbols-outlined">account_circle</span>
+        {routes.map((route) => (
+          <NavLink key={route.path} to={route.path}>
+            {route.label}
+          </NavLink>
+        ))}
 
-                {user && (
-                    <span className='user-info'>
-                        <Link to={`user/${user._id}`}>
-                            {user.imgUrl && <img src={user.imgUrl} />}
-                            {user.fullname}
-                        </Link>
-                        <span className='score'>{user.score?.toLocaleString()}</span>
-                        <button onClick={onLogout}>Logout</button>
-                    </span>
-                )}
-                {!user && (
-                    <section className='user-info'>
-                        <LoginSignup onLogin={onLogin} onSignup={onSignup} />
-                    </section>
-                )}
-            </nav>
-            <hr/>
-        </header>
-    )
+        {user && (
+          <span className="user-info">
+            <Link to={`user/${user._id}`}>
+              {user.imgUrl && <img src={user.imgUrl} />}
+              {user.fullname}
+            </Link>
+            <span className="score">{user.score?.toLocaleString()}</span>
+            <button onClick={onLogout}>Logout</button>
+          </span>
+        )}
+        {!user && (
+          <section className="user-info">
+            <LoginSignup onLogin={onLogin} onSignup={onSignup} />
+          </section>
+        )}
+      </nav>
+      <hr />
+    </header>
+  );
 }
--- frontend/src/root-cmp.jsx
+++ frontend/src/root-cmp.jsx
@@ -1,28 +1,32 @@
-import React from 'react'
-import { Routes, Route } from 'react-router'
+import React from "react";
+import { Routes, Route } from "react-router";
 
-import routes from './routes'
+import routes from "./routes";
 
-import { AppHeader } from './cmps/app-header'
-import { AppFooter } from './cmps/app-footer'
-import { UserDetails } from './pages/user-details'
-import { GigDetails } from './pages/gig-details'
+import { AppHeader } from "./cmps/app-header";
+import { AppFooter } from "./cmps/app-footer";
+import { UserDetails } from "./pages/user-details";
+import { GigDetails } from "./pages/gig-details";
 
 export function RootCmp() {
-
-    return (
-        <div className='main-container'>
-            <AppHeader />
-            <main>
-                <Routes>
-                    {routes.map(route => <Route key={route.path} exact={true} element={route.component} path={route.path} />)}
-                    <Route path="user/:id" element={<UserDetails />} />
-                    <Route path="gig/:id" element={<GigDetails />} />
-                </Routes>
-            </main>
-            <AppFooter />
-        </div>
-    )
+  return (
+    <div>
+      <AppHeader />
+      <main className="main-layout">
+        <Routes>
+          {routes.map((route) => (
+            <Route
+              key={route.path}
+              exact={true}
+              element={route.component}
+              path={route.path}
+            />
+          ))}
+          <Route path="user/:id" element={<UserDetails />} />
+          <Route path="gig/:id" element={<GigDetails />} />
+        </Routes>
+      </main>
+      <AppFooter />
+    </div>
+  );
 }
-
-
