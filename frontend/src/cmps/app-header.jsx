74953126005cb1613392aab01de32c370f556e16
Lior Geni <liorgeni@gmail.com>
2023-01-19 05:33:09 +0200

    fonts, header $ footer

--- frontend/src/cmps/app-header.jsx
+++ frontend/src/cmps/app-header.jsx
@@ -1,84 +1,153 @@
-import { Link, NavLink } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import routes from '../routes'
-import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
-import { login, logout, signup } from '../store/user.actions.js'
-import { LoginSignup } from './login-signup.jsx'
+import { Link, NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import routes from "../routes";
+import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service";
+import { login, logout, signup } from "../store/user.actions.js";
+import { LoginSignup } from "./login-signup.jsx";
 
 export function AppHeader() {
-    const user = useSelector((storeState) => storeState.userModule.user)
+  const user = useSelector((storeState) => storeState.userModule.user);
 
-    async function onLogin(credentials) {
-        try {
-            const user = await login(credentials)
-            showSuccessMsg(`Welcome: ${user.fullname}`)
-        } catch (err) {
-            showErrorMsg('Cannot login')
-        }
+  async function onLogin(credentials) {
+    try {
+      const user = await login(credentials);
+      showSuccessMsg(`Welcome: ${user.fullname}`);
+    } catch (err) {
+      showErrorMsg("Cannot login");
     }
-    async function onSignup(credentials) {
-        try {
-            const user = await signup(credentials)
-            showSuccessMsg(`Welcome new user: ${user.fullname}`)
-        } catch (err) {
-            showErrorMsg('Cannot signup')
-        }
+  }
+  async function onSignup(credentials) {
+    try {
+      const user = await signup(credentials);
+      showSuccessMsg(`Welcome new user: ${user.fullname}`);
+    } catch (err) {
+      showErrorMsg("Cannot signup");
     }
-    async function onLogout() {
-        try {
-            await logout()
-            showSuccessMsg(`Bye now`)
-        } catch (err) {
-            showErrorMsg('Cannot logout')
-        }
+  }
+  async function onLogout() {
+    try {
+      await logout();
+      showSuccessMsg(`Bye now`);
+    } catch (err) {
+      showErrorMsg("Cannot logout");
     }
+  }
 
-    return (
-        <header className='app-header'>
-            <nav className='flex'>
-                <h1 className='header-logo'>Logo</h1>
-                <input type='text' className='search-box' placeholder='What service are you looking for today?'></input>
-                <div className='search-icon-box'>
-                    <span class='material-symbols-outlined search-icon'>search</span>
-                </div>
-                <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.5 20.167h5M4.295 11.87l-1.562 1.563a3.07 3.07 0 00-.9 2.171v0c0 .678.55 1.228 1.229 1.228h15.877c.678 0 1.228-.55 1.228-1.228v0a3.07 3.07 0 00-.9-2.171l-1.562-1.563a2.975 2.975 0 01-.872-2.104v-2.1A5.833 5.833 0 0011 1.833v0a5.833 5.833 0 00-5.833 5.834v2.1c0 .79-.314 1.546-.872 2.104z" stroke="#74767e" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
-                <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M.833 9.834v5A1.667 1.667 0 002.5 16.5h15a1.666 1.666 0 001.667-1.666v-5" stroke="#74767e" stroke-width="2" stroke-miterlimit="10" stroke-linecap="square"/><path d="M19.167 5.667v-2.5A1.667 1.667 0 0017.5 1.5h-15A1.667 1.667 0 00.833 3.167v2.5l9.167 5 9.167-5z" stroke="#74767e" stroke-width="2" stroke-miterlimit="10" stroke-linecap="square"/></svg>
-                {/* <span class="material-symbols-outlined">
+  return (
+    <header className="app-header main-container full">
+      <nav className="flex ">
+        <Link className="header-logo" to={`/`}>
+          <svg
+            width="89"
+            height="27"
+            viewBox="0 0 89 27"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            class=""
+          >
+            <g fill="#404145">
+              <path
+                d="m81.6 13.1h-3.1c-2 0-3.1 1.5-3.1 4.1v9.3h-6v-13.4h-2.5c-2 0-3.1 1.5-3.1 4.1v9.3h-6v-18.4h6v2.8c1-2.2 2.3-2.8 4.3-2.8h7.3v2.8c1-2.2 2.3-2.8 4.3-2.8h2zm-25.2 5.6h-12.4c.3 2.1 1.6 3.2 3.7 3.2 1.6 0 2.7-.7 3.1-1.8l5.3 1.5c-1.3 3.2-4.5 5.1-8.4 5.1-6.5 0-9.5-5.1-9.5-9.5 0-4.3 2.6-9.4 9.1-9.4 6.9 0 9.2 5.2 9.2 9.1 0 .9 0 1.4-.1 1.8zm-5.7-3.5c-.1-1.6-1.3-3-3.3-3-1.9 0-3 .8-3.4 3zm-22.9 11.3h5.2l6.6-18.3h-6l-3.2 10.7-3.2-10.8h-6zm-24.4 0h5.9v-13.4h5.7v13.4h5.9v-18.4h-11.6v-1.1c0-1.2.9-2 2.2-2h3.5v-5h-4.4c-4.3 0-7.2 2.7-7.2 6.6v1.5h-3.4v5h3.4z"
+                class=""
+              ></path>
+            </g>
+            <g fill="#1dbf73">
+              <path d="m85.3 27c2 0 3.7-1.7 3.7-3.7s-1.7-3.7-3.7-3.7-3.7 1.7-3.7 3.7 1.7 3.7 3.7 3.7z"></path>
+            </g>
+          </svg>
+        </Link>
+        <input
+          type="text"
+          className="search-box"
+          placeholder="What service are you looking for today?"
+        ></input>
+        <div className="search-icon-box">
+          <span class="material-symbols-outlined search-icon">search</span>
+        </div>
+        <ul className="user-nav flex clean-list align-center space-between">
+          <li>
+            <p>Fiverr Buisness</p>
+          </li>
+          <li>
+            <svg
+              width="22"
+              height="22"
+              viewBox="0 0 22 22"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M8.5 20.167h5M4.295 11.87l-1.562 1.563a3.07 3.07 0 00-.9 2.171v0c0 .678.55 1.228 1.229 1.228h15.877c.678 0 1.228-.55 1.228-1.228v0a3.07 3.07 0 00-.9-2.171l-1.562-1.563a2.975 2.975 0 01-.872-2.104v-2.1A5.833 5.833 0 0011 1.833v0a5.833 5.833 0 00-5.833 5.834v2.1c0 .79-.314 1.546-.872 2.104z"
+                stroke="#74767e"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </li>
+          <li>
+            <svg
+              width="20"
+              height="18"
+              viewBox="0 0 20 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M.833 9.834v5A1.667 1.667 0 002.5 16.5h15a1.666 1.666 0 001.667-1.666v-5"
+                stroke="#74767e"
+                stroke-width="2"
+                stroke-miterlimit="10"
+                stroke-linecap="square"
+              />
+              <path
+                d="M19.167 5.667v-2.5A1.667 1.667 0 0017.5 1.5h-15A1.667 1.667 0 00.833 3.167v2.5l9.167 5 9.167-5z"
+                stroke="#74767e"
+                stroke-width="2"
+                stroke-miterlimit="10"
+                stroke-linecap="square"
+              />
+            </svg>
+          </li>
+
+          {/* <span class="material-symbols-outlined">
 notifications
 </span>
 <span class="material-symbols-outlined">
 mail
 </span> */}
-<span class="material-symbols-outlined">
-favorite
-</span>
-<span>Orders</span>
-<span class="material-symbols-outlined">
-account_circle
-</span>
-                {routes.map((route) => (
-                    <NavLink key={route.path} to={route.path}>
-                        {route.label}
-                    </NavLink>
-                ))}
+          <li>
+            <span class="material-symbols-outlined">favorite</span>{" "}
+          </li>
 
-                {user && (
-                    <span className='user-info'>
-                        <Link to={`user/${user._id}`}>
-                            {user.imgUrl && <img src={user.imgUrl} />}
-                            {user.fullname}
-                        </Link>
-                        <span className='score'>{user.score?.toLocaleString()}</span>
-                        <button onClick={onLogout}>Logout</button>
-                    </span>
-                )}
-                {!user && (
-                    <section className='user-info'>
-                        <LoginSignup onLogin={onLogin} onSignup={onSignup} />
-                    </section>
-                )}
-            </nav>
-            <hr/>
-        </header>
-    )
+          <li>
+            <span>Orders</span>
+          </li>
+
+          <li>
+            <span class="material-symbols-outlined">account_circle</span>{" "}
+          </li>
+
+          {user && (
+            <span className="user-info">
+              <Link to={`user/${user._id}`}>
+                {user.imgUrl && <img src={user.imgUrl} />}
+                {user.fullname}
+              </Link>
+              <span className="score">{user.score?.toLocaleString()}</span>
+              <button onClick={onLogout}>Logout</button>
+            </span>
+          )}
+        </ul>
+
+        {/* {!user && (
+          <section className="user-info">
+            <LoginSignup onLogin={onLogin} onSignup={onSignup} />
+          </section>
+        )} */}
+      </nav>
+
+      <hr className="full" />
+    </header>
+  );
 }
--- frontend/src/pages/home-page.jsx
+++ frontend/src/pages/home-page.jsx
@@ -1,29 +1,44 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import logo from '../assets/img/logo.png'
-import { CHANGE_COUNT } from '../store/user.reducer'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import logo from "../assets/img/logo.png";
+import { CHANGE_COUNT } from "../store/user.reducer";
+import routes from "../routes";
+import { Link, NavLink } from "react-router-dom";
 
 export function HomePage() {
-    const dispatch = useDispatch()
-    const count = useSelector(storeState => storeState.userModule.count)
+  const dispatch = useDispatch();
+  const count = useSelector((storeState) => storeState.userModule.count);
 
-    function changeCount(diff) {
-        console.log('Changing count by:', diff);
-        dispatch({ type: CHANGE_COUNT, diff })
-    }
+  function changeCount(diff) {
+    console.log("Changing count by:", diff);
+    dispatch({ type: CHANGE_COUNT, diff });
+  }
 
-    return (
-        <section>
-            <img src={logo} alt="Logo" style={{ maxWidth: '300px' }} />
-            <h2>
-                Count {count}
-                <button onClick={() => {
-                    changeCount(1)
-                }}>+</button>
-                <button onClick={() => {
-                    changeCount(10)
-                }}>+10</button>
-            </h2 >
-        </section >
-    )
+  return (
+    <section>
+      {/* <img src={logo} alt="Logo" style={{ maxWidth: "300px" }} />
+      <h2>
+        Count {count}
+        <button
+          onClick={() => {
+            changeCount(1);
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            changeCount(10);
+          }}
+        >
+          +10
+        </button>
+      </h2> */}
+      {routes.map((route) => (
+        <NavLink key={route.path} to={route.path}>
+          {route.label} |||
+        </NavLink>
+      ))}
+    </section>
+  );
 }
