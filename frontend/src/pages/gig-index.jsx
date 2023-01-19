9a2eaaae56f73379add69931bada1372be6bdfee
Lior Geni <liorgeni@gmail.com>
2023-01-19 15:08:33 +0200

    footer, bug fix

--- frontend/src/pages/gig-index.jsx
+++ frontend/src/pages/gig-index.jsx
@@ -1,71 +1,83 @@
-import { useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import { loadGigs, addGig, updateGig, removeGig, addToGigt } from '../store/gig.actions.js'
-import {Link} from 'react-router-dom'
-import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service.js'
-import { gigService } from '../services/gig.service.js'
-import { GigPreview } from '../cmps/gig-preview.jsx'
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import {
+  loadGigs,
+  addGig,
+  updateGig,
+  removeGig,
+  addToCart,
+} from "../store/gig.actions.js";
+import { Link } from "react-router-dom";
+import { showSuccessMsg, showErrorMsg } from "../services/event-bus.service.js";
+import { gigService } from "../services/gig.service.js";
+import { GigPreview } from "../cmps/gig-preview.jsx";
 
 export function GigIndex() {
+  const gigs = useSelector((storeState) => storeState.gigModule.gigs);
 
-    const gigs = useSelector(storeState => storeState.gigModule.gigs)
+  useEffect(() => {
+    loadGigs();
+  }, []);
 
-    useEffect(() => {
-        loadGigs()
-    }, [])
-
-    async function onRemoveGig(gigId) {
-        try {
-            await removeGig(gigId)
-            showSuccessMsg('Gig removed')            
-        } catch (err) {
-            showErrorMsg('Cannot remove gig')
-        }
+  async function onRemoveGig(gigId) {
+    try {
+      await removeGig(gigId);
+      showSuccessMsg("Gig removed");
+    } catch (err) {
+      showErrorMsg("Cannot remove gig");
     }
+  }
 
-    async function onAddGig() {
-        const gig = gigService.getEmptyGig()
-        gig.vendor = prompt('Vendor?')
-        try {
-            const savedGig = await addGig(gig)
-            showSuccessMsg(`Gig added (id: ${savedGig._id})`)
-        } catch (err) {
-            showErrorMsg('Cannot add gig')
-        }        
+  async function onAddGig() {
+    const gig = gigService.getEmptyGig();
+    gig.vendor = prompt("Vendor?");
+    try {
+      const savedGig = await addGig(gig);
+      showSuccessMsg(`Gig added (id: ${savedGig._id})`);
+    } catch (err) {
+      showErrorMsg("Cannot add gig");
     }
+  }
 
-    async function onUpdateGig(gig) {
-        const price = +prompt('New price?')
-        const gigToSave = { ...gig, price }
-        try {
-            const savedGig = await updateGig(gigToSave)
-            showSuccessMsg(`Gig updated, new price: ${savedGig.price}`)
-        } catch (err) {
-            showErrorMsg('Cannot update gig')
-        }        
+  async function onUpdateGig(gig) {
+    const price = +prompt("New price?");
+    const gigToSave = { ...gig, price };
+    try {
+      const savedGig = await updateGig(gigToSave);
+      showSuccessMsg(`Gig updated, new price: ${savedGig.price}`);
+    } catch (err) {
+      showErrorMsg("Cannot update gig");
     }
+  }
 
-    function onAddToGigt(gig){
-        console.log(`Adding ${gig.vendor} to Gigt`)
-        addToGigt(gig)
-        showSuccessMsg('Added to Gigt')
-    }
+  function onAddToCart(gig) {
+    console.log(`Adding ${gig.vendor} to Gigt`);
+    addToCart(gig);
+    showSuccessMsg("Added to Gigt");
+  }
 
-    function onAddGigMsg(gig) {
-        console.log(`TODO Adding msg to gig`)
-    }
+  function onAddGigMsg(gig) {
+    console.log(`TODO Adding msg to gig`);
+  }
 
-    return (
-        <div className='gig-index'>
-            <main>
-            <h2>Most popular Gigs</h2>
-                {/* <button onClick={onAddGig}>Add Gig ⛐</button> */}
-                <ul className="gig-list">
-                    {gigs.map((gig , idx) =>
-                       <GigPreview id={idx} gig={gig} onRemoveGig={onRemoveGig} onUpdateGig={onUpdateGig} onAddGigMsg={onAddGigMsg} onAddToGigt={onAddToGigt}/>)
-                    }
-                </ul>
-            </main>
-        </div>
-    )
+  return (
+    <div className="gig-index">
+      <main>
+        <h2>Most popular Gigs</h2>
+        {/* <button onClick={onAddGig}>Add Gig ⛐</button> */}
+        <ul className="gig-list">
+          {gigs.map((gig, idx) => (
+            <GigPreview
+              id={idx}
+              gig={gig}
+              onRemoveGig={onRemoveGig}
+              onUpdateGig={onUpdateGig}
+              onAddGigMsg={onAddGigMsg}
+              onAddToCart={onAddToCart}
+            />
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
 }
--- frontend/src/store/gig.reducer.js
+++ frontend/src/store/gig.reducer.js
@@ -1,54 +1,60 @@
-export const SET_GIGS = 'SET_GIGS'
-export const REMOVE_GIG = 'REMOVE_GIG'
-export const ADD_GIG = 'ADD_GIG'
-export const UPDATE_GIG = 'UPDATE_GIG'
-export const ADD_TO_GIGT = 'ADD_TO_GIGT'
-export const CLEAR_GIGT = 'CLEAR_GIGT'
-export const UNDO_REMOVE_GIG = 'UNDO_REMOVE_GIG'
-export const REMOVE_FROM_GIGT = 'REMOVE_FROM_GIGT'
+export const SET_GIGS = "SET_GIGS";
+export const REMOVE_GIG = "REMOVE_GIG";
+export const ADD_GIG = "ADD_GIG";
+export const UPDATE_GIG = "UPDATE_GIG";
+export const ADD_TO_CART = "ADD_TO_CART";
+export const CLEAR_CART = "CLEAR_CART";
+export const UNDO_REMOVE_GIG = "UNDO_REMOVE_GIG";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
 
 const initialState = {
-    gigs: [],
-    gigt: [],
-    lastRemovedGig: null
-}
+  gigs: [],
+  cart: [],
+  lastRemovedGig: null,
+};
 
 export function gigReducer(state = initialState, action) {
-    var newState = state
-    var gigs
-    var gigt
-    switch (action.type) {
-        case SET_GIGS:
-            newState = { ...state, gigs: action.gigs }
-            break
-        case REMOVE_GIG:
-            const lastRemovedGig = state.gigs.find(gig => gig._id === action.gigId)
-            gigs = state.gigs.filter(gig => gig._id !== action.gigId)
-            newState = { ...state, gigs, lastRemovedGig }
-            break
-        case ADD_GIG:
-            newState = { ...state, gigs: [...state.gigs, action.gig] }
-            break
-        case UPDATE_GIG:
-            gigs = state.gigs.map(gig => (gig._id === action.gig._id) ? action.gig : gig)
-            newState = { ...state, gigs }
-            break
-        case ADD_TO_GIGT:
-            newState = { ...state, gigt: [...state.gigt, action.gig] }
-            break
-        case REMOVE_FROM_GIGT:
-            gigt = state.gigt.filter(gig => gig._id !== action.gigId)
-            newState = { ...state, gigt }
-            break
-        case CLEAR_GIGT:
-            newState = { ...state, gigt: [] }
-            break
-        case UNDO_REMOVE_GIG:
-            if (state.lastRemovedGig) {
-                newState = { ...state, gigs: [...state.gigs, state.lastRemovedGig], lastRemovedGig: null }
-            }
-            break
-        default:
-    }
-    return newState
+  var newState = state;
+  var gigs;
+  var cart;
+  switch (action.type) {
+    case SET_GIGS:
+      newState = { ...state, gigs: action.gigs };
+      break;
+    case REMOVE_GIG:
+      const lastRemovedGig = state.gigs.find((gig) => gig._id === action.gigId);
+      gigs = state.gigs.filter((gig) => gig._id !== action.gigId);
+      newState = { ...state, gigs, lastRemovedGig };
+      break;
+    case ADD_GIG:
+      newState = { ...state, gigs: [...state.gigs, action.gig] };
+      break;
+    case UPDATE_GIG:
+      gigs = state.gigs.map((gig) =>
+        gig._id === action.gig._id ? action.gig : gig
+      );
+      newState = { ...state, gigs };
+      break;
+    case ADD_TO_CART:
+      newState = { ...state, cart: [...state.cart, action.gig] };
+      break;
+    case REMOVE_FROM_CART:
+      cart = state.cart.filter((gig) => gig._id !== action.gigId);
+      newState = { ...state, cart };
+      break;
+    case CLEAR_CART:
+      newState = { ...state, cart: [] };
+      break;
+    case UNDO_REMOVE_GIG:
+      if (state.lastRemovedGig) {
+        newState = {
+          ...state,
+          gigs: [...state.gigs, state.lastRemovedGig],
+          lastRemovedGig: null,
+        };
+      }
+      break;
+    default:
+  }
+  return newState;
 }
