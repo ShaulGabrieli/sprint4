4fbfe39c300884ad5db1c69f89bb7e71ab80a381
Lior Geni <liorgeni@gmail.com>
2023-01-21 00:27:08 +0200

    main-nav hover fix

--- frontend/src/cmps/app-header.jsx
+++ frontend/src/cmps/app-header.jsx
@@ -35,7 +35,7 @@ export function AppHeader() {
 
   return (
     <header className="app-header main-container full sticky">
-      <nav className="flex ">
+      <nav className="flex  ">
         <Link className="header-logo" to={`/`}>
           <svg
             width="89"
@@ -43,13 +43,9 @@ export function AppHeader() {
             viewBox="0 0 89 27"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
-        
           >
             <g fill="#404145">
-              <path
-                d="m81.6 13.1h-3.1c-2 0-3.1 1.5-3.1 4.1v9.3h-6v-13.4h-2.5c-2 0-3.1 1.5-3.1 4.1v9.3h-6v-18.4h6v2.8c1-2.2 2.3-2.8 4.3-2.8h7.3v2.8c1-2.2 2.3-2.8 4.3-2.8h2zm-25.2 5.6h-12.4c.3 2.1 1.6 3.2 3.7 3.2 1.6 0 2.7-.7 3.1-1.8l5.3 1.5c-1.3 3.2-4.5 5.1-8.4 5.1-6.5 0-9.5-5.1-9.5-9.5 0-4.3 2.6-9.4 9.1-9.4 6.9 0 9.2 5.2 9.2 9.1 0 .9 0 1.4-.1 1.8zm-5.7-3.5c-.1-1.6-1.3-3-3.3-3-1.9 0-3 .8-3.4 3zm-22.9 11.3h5.2l6.6-18.3h-6l-3.2 10.7-3.2-10.8h-6zm-24.4 0h5.9v-13.4h5.7v13.4h5.9v-18.4h-11.6v-1.1c0-1.2.9-2 2.2-2h3.5v-5h-4.4c-4.3 0-7.2 2.7-7.2 6.6v1.5h-3.4v5h3.4z"
-          
-              ></path>
+              <path d="m81.6 13.1h-3.1c-2 0-3.1 1.5-3.1 4.1v9.3h-6v-13.4h-2.5c-2 0-3.1 1.5-3.1 4.1v9.3h-6v-18.4h6v2.8c1-2.2 2.3-2.8 4.3-2.8h7.3v2.8c1-2.2 2.3-2.8 4.3-2.8h2zm-25.2 5.6h-12.4c.3 2.1 1.6 3.2 3.7 3.2 1.6 0 2.7-.7 3.1-1.8l5.3 1.5c-1.3 3.2-4.5 5.1-8.4 5.1-6.5 0-9.5-5.1-9.5-9.5 0-4.3 2.6-9.4 9.1-9.4 6.9 0 9.2 5.2 9.2 9.1 0 .9 0 1.4-.1 1.8zm-5.7-3.5c-.1-1.6-1.3-3-3.3-3-1.9 0-3 .8-3.4 3zm-22.9 11.3h5.2l6.6-18.3h-6l-3.2 10.7-3.2-10.8h-6zm-24.4 0h5.9v-13.4h5.7v13.4h5.9v-18.4h-11.6v-1.1c0-1.2.9-2 2.2-2h3.5v-5h-4.4c-4.3 0-7.2 2.7-7.2 6.6v1.5h-3.4v5h3.4z"></path>
             </g>
             <g fill="#1dbf73">
               <path d="m85.3 27c2 0 3.7-1.7 3.7-3.7s-1.7-3.7-3.7-3.7-3.7 1.7-3.7 3.7 1.7 3.7 3.7 3.7z"></path>
@@ -147,37 +143,41 @@ mail
         )} */}
       </nav>
 
-      <hr className="full " />
-      <ul className="categories-nav clean list flex space-between jusitfy-center ">
-        <li>
-          <a href="">Graphics & Design</a>
-        </li>
-        <li>
-          <a href="">Digital Marketing</a>
-        </li>
-        <li>
-          <a href="">Writing & Translation</a>
-        </li>
-        <li>
-          <a href="">Video & Animation</a>
-        </li>
-        <li>
-          <a href="">Music & Audio</a>
-        </li>
-        <li>
-          <a href="">Programming & Tech</a>
-        </li>
-        <li>
-          <a href="">Business</a>
-        </li>
-        <li>
-          <a href="">Lifestyle</a>
-        </li>
-        <li>
-          <a href="">Trending</a>
-        </li>
-      </ul>
-      <hr className="full " />
+      <hr className="full" />
+      <nav className="main-nav">
+        <ul className="clean list flex space-between jusitfy-center ">
+          <li>
+            <a className="active" href="/">
+              Graphics & Design
+            </a>
+          </li>
+          <li>
+            <a href="">Digital Marketing</a>
+          </li>
+          <li>
+            <a href="">Writing & Translation</a>
+          </li>
+          <li>
+            <a href="">Video & Animation</a>
+          </li>
+          <li>
+            <a href="">Music & Audio</a>
+          </li>
+          <li>
+            <a href="">Programming & Tech</a>
+          </li>
+          <li>
+            <a href="">Business</a>
+          </li>
+          <li>
+            <a href="">Lifestyle</a>
+          </li>
+          <li>
+            <a href="">Trending</a>
+          </li>
+        </ul>
+        <hr className="full " />
+      </nav>
     </header>
   );
 }
