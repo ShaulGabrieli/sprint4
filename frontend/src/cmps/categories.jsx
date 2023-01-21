9ba59150ac73f5becd9c81de46518c74e4436067
Lior Geni <liorgeni@gmail.com>
2023-01-22 01:20:52 +0200

    bottom-nav

---
+++ frontend/src/cmps/categories.jsx
@@ -0,0 +1,24 @@
+export function Categories() {
+  return (
+    <section className="bottom-nav-container">
+      <div className="cat">
+        <p>asdasd</p>
+      </div>
+      <div className="cat">
+        <p>asdasd</p>
+      </div>
+      <div className="cat">
+        <p>asdasd</p>
+      </div>
+      <div className="cat">
+        <p>asdasd</p>
+      </div>
+      <div className="cat">
+        <p>asdasd</p>
+      </div>
+
+      {/*
+        <div class="hero-info flex"></div> */}
+    </section>
+  );
+}
