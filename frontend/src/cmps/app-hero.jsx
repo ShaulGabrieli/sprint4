6739d2a58d36566d8b3e886aac6451f6c885d755
Lior Geni <liorgeni@gmail.com>
2023-01-22 11:16:40 +0200

    homepage fix

--- frontend/src/cmps/app-hero.jsx
+++ frontend/src/cmps/app-hero.jsx
@@ -1,4 +1,7 @@
 import hero1 from "../assets/img/hero1.webp";
+// import hero2 from "../assets/img/hero2.webp";
+// import hero3 from "../assets/img/hero3.webp";
+// import hero4 from "../assets/img/hero4.webp";
 
 export function AppHero() {
   return (
