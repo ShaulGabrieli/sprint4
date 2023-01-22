0d298b51de03ad377fefff683c1694a65a080821
Lior Geni <liorgeni@gmail.com>
2023-01-22 08:47:24 +0200

    homepage-carousel

--- frontend/src/pages/home-page.jsx
+++ frontend/src/pages/home-page.jsx
@@ -1,14 +1,12 @@
 import React from "react";
+import { PopularServices } from "../cmps/popular-services.jsx";
 import { SellingAd } from "../cmps/selling-ad.jsx";
 import { MarketCategories } from "../cmps/market-categories.jsx";
 
 export function HomePage() {
   return (
     <section className="home-page  full">
-      <section className="main-container popular-services">
-        <h2>Popular professional services</h2>
-      </section>
-
+      {/* <PopularServices /> */}
       <SellingAd />
       <MarketCategories />
     </section>
