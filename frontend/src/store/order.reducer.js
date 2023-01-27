61f7654a59ba46cfd1b0435d5096fa44767455aa
Lior Geni <liorgeni@gmail.com>
2023-01-27 15:20:46 +0200

    homepage, pagedetalis css

--- frontend/src/store/order.reducer.js
+++ frontend/src/store/order.reducer.js
@@ -1,44 +1,48 @@
-export const SET_USER_ORDERS = 'SET_USER_ORDERS'
-export const REMOVE_ORDER = 'REMOVE_ORDER'
-export const ADD_ORDER = 'ADD_ORDER'
-export const SET_CURRENT_ORDER = 'SET_CURRENT_ORDER'
-export const UPDATE_ORDER = 'UPDATE_ORDER'
-export const SET_SELLER_ORDERS = 'SET_SELLER_ORDERS'
+export const SET_USER_ORDERS = "SET_USER_ORDERS";
+export const REMOVE_ORDER = "REMOVE_ORDER";
+export const ADD_ORDER = "ADD_ORDER";
+export const SET_CURRENT_ORDER = "SET_CURRENT_ORDER";
+export const UPDATE_ORDER = "UPDATE_ORDER";
+export const SET_SELLER_ORDERS = "SET_SELLER_ORDERS";
 
 const initialState = {
-    order: {},
-    userOrders: null,
-    lastRemovedOrder: null,
-    sellerOrders: null
-}
+  order: {},
+  userOrders: null,
+  lastRemovedOrder: null,
+  sellerOrders: null,
+};
 
 export function orderReducer(state = initialState, action) {
-    var newState = state
-    var userOrders
-    var sellerOrders
-    console.log('action', action);
-    switch (action.type) {
-        case SET_USER_ORDERS:
-            newState = { ...state, userOrders: action.orders }
-            break
-        case SET_SELLER_ORDERS:
-            newState = { ...state, sellerOrders: action.orders }
-            break
-        case REMOVE_ORDER:
-            newState = { ...state, order: {} }
-            break
-        case ADD_ORDER:
-            newState = { ...state, order: action.order }
-            break
-        case SET_CURRENT_ORDER:
-            newState = { ...state, order: action.order }
-            break
-        case UPDATE_ORDER:
-            userOrders = state.userOrders.map((order) => (order._id === action.order._id ? action.order : order))
-            sellerOrders = state.sellerOrders.map((order) => (order._id === action.order._id ? action.order : order))
-            newState = { ...state, userOrders, sellerOrders }
-            break
-        default:
-    }
-    return newState
+  var newState = state;
+  var userOrders;
+  var sellerOrders;
+  // console.log('action', action);
+  switch (action.type) {
+    case SET_USER_ORDERS:
+      newState = { ...state, userOrders: action.orders };
+      break;
+    case SET_SELLER_ORDERS:
+      newState = { ...state, sellerOrders: action.orders };
+      break;
+    case REMOVE_ORDER:
+      newState = { ...state, order: {} };
+      break;
+    case ADD_ORDER:
+      newState = { ...state, order: action.order };
+      break;
+    case SET_CURRENT_ORDER:
+      newState = { ...state, order: action.order };
+      break;
+    case UPDATE_ORDER:
+      userOrders = state.userOrders.map((order) =>
+        order._id === action.order._id ? action.order : order
+      );
+      sellerOrders = state.sellerOrders.map((order) =>
+        order._id === action.order._id ? action.order : order
+      );
+      newState = { ...state, userOrders, sellerOrders };
+      break;
+    default:
+  }
+  return newState;
 }
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,31 +1,28 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers } from "redux";
 
-import { gigReducer } from './gig.reducer.js'
-import { userReducer } from './user.reducer.js'
-import { reviewReducer } from './review.reducer'
-import { systemReducer } from './system.reducer'
-import { orderReducer } from './order.reducer'
-import { filterReducer } from './filter.reducer.js'
+import { gigReducer } from "./gig.reducer.js";
+import { userReducer } from "./user.reducer.js";
+import { reviewReducer } from "./review.reducer";
+import { systemReducer } from "./system.reducer";
+import { orderReducer } from "./order.reducer";
+import { filterReducer } from "./filter.reducer.js";
 
 const rootReducer = combineReducers({
-    gigModule: gigReducer,
-    userModule: userReducer,
-    systemModule: systemReducer,
-    reviewModule: reviewReducer,
-    orderModule: orderReducer,
-    filterModule: filterReducer,
-})
+  gigModule: gigReducer,
+  userModule: userReducer,
+  systemModule: systemReducer,
+  reviewModule: reviewReducer,
+  orderModule: orderReducer,
+  filterModule: filterReducer,
+});
 
+const middleware = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+  : undefined;
+export const store = createStore(rootReducer, middleware);
 
-const middleware = (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : undefined
-export const store = createStore(rootReducer, middleware)
-
-
-store.subscribe(() => {
-    console.log('**** Store state changed: ****')
-    console.log('storeState:\n', store.getState())
-    console.log('*******************************')
-})
-
-
-
+// store.subscribe(() => {
+//     console.log('**** Store state changed: ****')
+//     console.log('storeState:\n', store.getState())
+//     console.log('*******************************')
+// })
