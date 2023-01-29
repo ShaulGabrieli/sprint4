da4f1e4a108d6d69087258722408e7aa6e95225d
Shaul Gabrieli <shaulcasper@gmail.com>
2023-01-29 22:34:10 +0200

    added a drop down menu on mobile

--- frontend/src/cmps/charts.jsx
+++ frontend/src/cmps/charts.jsx
@@ -7,23 +7,21 @@ const BASE_CHARTS_URL = "https://charts.mongodb.com/charts-sprint4-txzkw"
 export function MyChart({ chartId, sellerId, height, width }) {
   const randDivId = utilService.makeId()
   const sdk = new ChartsEmbedSDK({
-    baseUrl: BASE_CHARTS_URL, // REPLACE with the Base URL from your Embed Chart dialog.
+    baseUrl: BASE_CHARTS_URL, 
   });
   const chart = sdk.createChart({
-    chartId: chartId, // REPLACE with the Chart ID from your Embed Chart dialog.
+    chartId: chartId, 
     height: height,
     width: width,
     filter: { "seller._id": sellerId },
     maxDataAge: 60,
     autoRefresh: true
-    // Additional options go here
   });
 
   useEffect(() => {
     chart.render(document.getElementById(randDivId))
   }, [])
   return (
-    // id="63d2e8cb-7df1-415a-8cc0-8dfade37b3a9"
     <div id={randDivId}></div>
   )
 }
